48a18baa8b1430856f4e08c7504ecb56a58ac324
CemMeric26 <meric.sefikogullari@metu.edu.tr>
2023-12-30 23:35:42 +0300

    udp  process segment and send ack func implemented

--- ceng435-socket/code/udp-part/udp_server.py
+++ ceng435-socket/code/udp-part/udp_server.py
@@ -1,13 +1,43 @@
 import socket
 import json
+import os
 
-def process_segment(segment):
+def process_segment(segment, output_directory):
     # Process received segment
-    pass
+    # Extract segment data
+    sequence_number = segment['sequence_number'] # get the value from the segment dictionary
+    data = segment['data']                       # get the value from the segment dictionary
+    is_last_segment = segment['is_last_segment'] # get the value from the segment dictionary
+
+    # Define the output file path 
+    output_file_path = os.path.join(output_directory, f"/output_{sequence_number}.obj")
+
+    # Write the segment data to the output file
+    with open(output_file_path, 'wb') as file:
+        file.write(data)
+
+    print(f"Processed segment {sequence_number}, Last Segment: {is_last_segment}")
+
+    return is_last_segment  # Return if the segment is the last segment, this means all segments are received
 
 def send_ack(udp_socket, client_address, sequence_number):
     # Send acknowledgment for received segment
-    pass
+    try:
+        # Create an acknowledgment message
+        # The function creates a dictionary ack_message with the sequence number 
+        # that was successfully received
+        ack_message = {
+            'acknowledged_sequence_number': sequence_number
+        }
+
+        # Serialize the acknowledgment message
+        # seriazlize it to json then encode it to bytes
+        serialized_ack = json.dumps(ack_message).encode('utf-8')
+
+        # Send the acknowledgment back to the client
+        udp_socket.sendto(serialized_ack, client_address)
+    except Exception as e:
+        print(f"Error sending acknowledgment: {e}")
 
 def start_server():
     local_ip = "127.0.0.1"
@@ -18,20 +48,25 @@ def start_server():
     print("UDP server up and listening")
 
     received_segments = set()
+    output_directory = "./received_files"  # Define the output directory
+
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
     while True:
         bytes_address_pair = udp_socket.recvfrom(1024)
-        segment = json.loads(bytes_address_pair[0])
+        segment = json.loads(bytes_address_pair[0].decode('utf-8'))
         address = bytes_address_pair[1]
 
         if segment["sequence_number"] not in received_segments:
-            process_segment(segment)
+            is_last_segment = process_segment(segment, output_directory)
             send_ack(udp_socket, address, segment["sequence_number"])
             received_segments.add(segment["sequence_number"])
 
-        # Implement logic to check for the last segment
-        # If the last segment is received, break the loop
-            break
+            if is_last_segment:
+                # break the loop if the last segment is received
+                print("Last segment received. Server is closing.")
+                break
 
     udp_socket.close()
 
