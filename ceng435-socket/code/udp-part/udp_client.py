48f1c780579bbfc81b3a1de6042e10118b361c88
CemMeric26 <meric.sefikogullari@metu.edu.tr>
2024-01-04 19:19:20 +0300

    gbn function timer dont work

--- ceng435-socket/code/udp-part/udp_client.py
+++ ceng435-socket/code/udp-part/udp_client.py
@@ -2,6 +2,28 @@ import socket
 import os
 import pickle
 import time
+import hashlib
+
+def calculate_checksum(data):
+    # Convert data to bytes if it is not already a bytes-like object
+    if isinstance(data, str):
+        # If it's a string, encode it to bytes
+        data = data.encode('utf-8')
+    elif isinstance(data, (bytearray, memoryview)):
+        # If it's a bytearray or memoryview (buffer protocol types), convert to bytes
+        data = bytes(data)
+    elif not isinstance(data, bytes):
+        # If it's not a bytes-like object at all, raise an error
+        raise TypeError("The data provided is not bytes or cannot be converted to bytes.")
+    
+    # Create an MD5 hash object
+    hash_obj = hashlib.md5()
+    
+    # Update the hash object with the data
+    hash_obj.update(data)
+    
+    # Return the hex digest of the data
+    return hash_obj.hexdigest()
 
 def create_segment_for_file(file_path, segment_size,file_id):
     """
@@ -25,7 +47,8 @@ def create_segment_for_file(file_path, segment_size,file_id):
                 'file_id': file_id, # identify the file
                 'sequence_number': sequence_number, # order of the segment
                 'data': data, # actual data
-                'is_last_segment': False # flag for the last segment
+                'is_last_segment': False, # flag for the last segment
+                # 'checksum': calculate_checksum(data) # checksum of the segment
             }
             all_segments.append(segment)
             sequence_number += 1
@@ -46,22 +69,17 @@ def send_segment(udp_socket, segment, server_address):
     except Exception as e:
         print(f"Error sending segment: {e}")
 
-def receive_ack(udp_socket, expected_seq_num, timeout=2):
+def receive_ack(udp_socket, expected_seq_num):
     # this function waits for an ack for the segment from the server
     # ack is expected for a specific segment
     try:
-        udp_socket.settimeout(timeout)
         while True:
             ack_data, _ = udp_socket.recvfrom(1024)  # buffer size
-            ack = pickle.loads(ack_data)
-            
+            ack = pickle.loads(ack_data)         
             # Check if the acknowledgment is for the expected segment
             if ack.get('acknowledged_sequence_number') == expected_seq_num:
                 print(f"Acknowledgment received for segment {expected_seq_num}")
                 return ack
-    except socket.timeout:
-        print(f"No acknowledgment received for segment {expected_seq_num}.")
-        return None
     except Exception as e:
         print(f"Error in receiving acknowledgment: {e}")
         return None
@@ -93,8 +111,74 @@ def interleave_segments(segments):
 
     return interleaved_segments
 
+def GBN_sender(udp_socket,server_address, base, next_seq_num, N, interleaved_segments, timer_start_time, timeout_duration):
+    
+    sent_segments = set()
+    acked_segments = set()
+    timer_start_time = None  # Initialize the timer to None
 
-def start_client(server_ip, server_port, window_size=10):
+    while base < len(interleaved_segments):
+        # Send segments that are within the window
+        # print(f"Acknowledgment received for segment {acked_seq_num}")
+        while next_seq_num < base + N and next_seq_num <= len(interleaved_segments):
+            segment = interleaved_segments[next_seq_num ]  # sequence numbers are 1-indexed
+            send_segment(udp_socket, segment, server_address)
+            sent_segments.add(segment["sequence_number"])
+
+            # print(f"Sending segment {seq_num}, Timer: {time.time() - timer_start_time:.2f}")
+            print(f"Current window: [{base}, {base + N - 1}]")
+            print(f"Sent segment {segment['sequence_number']}")
+
+            if base == next_seq_num:
+                # Start timer upon sending the first segment in the window
+                timer_start_time = time.time()
+            next_seq_num += 1
+
+        # Check for ACKs
+        ack = receive_ack(udp_socket, base)
+        if ack is not None:
+            acked_seq_num = ack['acknowledged_sequence_number']
+            if acked_seq_num in sent_segments:
+                print(f"Received ACK for segment {acked_seq_num}")
+                acked_segments.add(acked_seq_num)
+                if acked_seq_num >= base:
+                    # Move the base up to the next unacknowledged segment
+                    while base in acked_segments:
+                        base += 1
+                        acked_segments.remove(base-1)
+                        # Restart timer upon receiving an ACK for the base segment
+                    
+                    timer_start_time = time.time()
+        
+        # Check for timeout
+        print(f"Timer timeout time: {time.time()-timer_start_time}")
+
+        if timer_start_time and ((time.time() - timer_start_time) > timeout_duration):
+            print(f"Timeout for segment {base}, resending...")
+            next_seq_num = base
+
+            timer_start_time = time.time()  # Restart timer for the oldest unacknowledged packet
+
+            # Resend all segments starting from 'base' up to 'next_seq_num'
+            for seq_num in range(base, min(base + N, len(interleaved_segments))):
+                segment = interleaved_segments[seq_num]
+                send_segment(udp_socket, segment, server_address)
+                print(f"Resent segment {segment['sequence_number']}")
+
+            # timer_start_time = time.time()  # Restart timer for the oldest unacknowledged packet
+            # Resend all segments starting from 'base' up to 'next_seq_num - 1'
+            """
+            for seq_num in range(base, next_seq_num):
+                segment = interleaved_segments[seq_num]  # sequence numbers are 1-indexed
+                send_segment(udp_socket, segment, server_address)
+                """
+
+
+    print("All segments have been acknowledged.")
+
+        
+
+def start_client(server_ip, server_port, window_size=100):
     # main function for the client
     # It sends file segments to the server, ensuring that the number of unacknowledged
     # segments does not exceed the window size.
@@ -130,9 +214,27 @@ def start_client(server_ip, server_port, window_size=10):
     interleaved_segments = interleave_segments(each_segments)
 
     # print(len(interleaved_segments))
+    base = 0
+    next_seq_num = 0
+    N = window_size  # window size
+
+    # Start timer for the oldest unacknowledged packet
+    timer_start_time = None
+    timeout_duration = 0.5  # Duration after which to consider a timeout has occurred
+
+    start_time = time.time()
+
+    GBN_sender(udp_socket,server_address, base, next_seq_num, N, interleaved_segments, timer_start_time, timeout_duration)
+
+    end_time = time.time()  # End time
+    elapsed_time = end_time - start_time
+    print(f"Total time taken for file transfer: {elapsed_time} seconds")
+    print(f"Average throughput: {FILE_COUNT * segment_size * 8 / elapsed_time} bits per second")
 
 
-    start_time = time.time()  # Start time
+
+    udp_socket.close()
+"""
     for segment in interleaved_segments:
         # print(f"Sending segment {segment['file_id']}-{segment['sequence_number']}")
         
@@ -144,12 +246,10 @@ def start_client(server_ip, server_port, window_size=10):
         sent_segments.add(segment["sequence_number"])
         acked_segments.add(segment["sequence_number"]) # when ack is received, add it to acked_segments
 
-    end_time = time.time()  # End time
-    elapsed_time = end_time - start_time
-    print(f"Total time taken for file transfer: {elapsed_time} seconds")
-    print(f"Average throughput: {FILE_COUNT * segment_size * 8 / elapsed_time} bits per second")
-    
-    udp_socket.close()
+"""
+
 
 if __name__ == "__main__":
-    start_client("server", 8000)
+    # IP = "127.0.0.1"
+    IP = "server"
+    start_client(IP, 8000)
--- ceng435-socket/code/udp-part/udp_server.py
+++ ceng435-socket/code/udp-part/udp_server.py
@@ -11,14 +11,15 @@ def process_segment(segment, received_segments):
 
     return segment['is_last_segment']
 
-def send_ack(udp_socket, client_address, sequence_number):
+def send_ack(udp_socket, client_address, sequence_number,checksum):
     # Send ack back to the client for a received segment.
     try:
         # Create an acknowledgment message
         # The function creates a dictionary ack_message with the sequence number 
         # that was successfully received
         ack_message = {
-            'acknowledged_sequence_number': sequence_number
+            'acknowledged_sequence_number': sequence_number,
+            'checksum': checksum
         }
 
         # Serialize the acknowledgment message
@@ -57,6 +58,41 @@ def verify_checksum(file_path, file_type, file_number):
     # Compare and return the result of checksum verification
     return our_checksum == expected_checksum
 
+def calculate_checksum(data):
+    # Convert data to bytes if it is not already a bytes-like object
+    if isinstance(data, str):
+        # If it's a string, encode it to bytes
+        data = data.encode('utf-8')
+    elif isinstance(data, (bytearray, memoryview)):
+        # If it's a bytearray or memoryview (buffer protocol types), convert to bytes
+        data = bytes(data)
+    elif not isinstance(data, bytes):
+        # If it's not a bytes-like object at all, raise an error
+        raise TypeError("The data provided is not bytes or cannot be converted to bytes.")
+    
+    # Create an MD5 hash object
+    hash_obj = hashlib.md5()
+    
+    # Update the hash object with the data
+    hash_obj.update(data)
+    
+    # Return the hex digest of the data
+    return hash_obj.hexdigest()
+def receive_segment(udp_socket):
+    # Receive a segment over UDP and return the deserialized segment.
+    try:
+        # The recvfrom method of the socket
+        # The recvfrom method returns a tuple with the serialized segment and the address of the sender
+        serialized_segment, address = udp_socket.recvfrom(1024)  # buffer size
+
+        # Deserialize the segment using pickle
+        segment = pickle.loads(serialized_segment)
+
+        # Return the deserialized segment and the address of the sender
+        return segment
+    except Exception as e:
+        print(f"Error receiving segment: {e}")
+        return None, None
 
 def reassemble_file(received_files, output_directory, file_id, total_files):
     """
@@ -83,20 +119,72 @@ def reassemble_file(received_files, output_directory, file_id, total_files):
     else:
         print(f"File {output_file_path} reassembled and saved, but failed verification.")
 
+def is_not_corrupt(segment):
+    # Placeholder function to check if a segment is not corrupt
+    hash_object_data = hashlib.md5()
+    # Update the hash object with the data
+    hash_object_data.update(segment['data'])
+    # Return the hex digest of the data
+    # hash_object_data.hexdigest() == segment['checksum']
+    return True
 
-def start_server():
-    local_ip = "server"
-    local_port = 8000
-    udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    udp_socket.bind((local_ip, local_port))
+def has_sequence_number(segment, expected_seq_num):
+    # Check if the segment has the expected sequence number
+    return segment['sequence_number'] == expected_seq_num
 
-    print("UDP server up and listening")
+
+def GBN_receiver(udp_socket):
 
     received_segments = {} # Dictionary to store received segments
     output_directory = "./received_files"  # Define the output directory
 
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
+
+    expected_seq_num = 0  # Start with expecting the first sequence number
+
+    while True:
+        # segment = receive_segment(udp_socket)
+        buffer_size = 10240
+        bytes_address_pair = udp_socket.recvfrom(buffer_size)
+        segment = pickle.loads(bytes_address_pair[0])  # Deserialize the segment using pickle  
+        address = bytes_address_pair[1]
+      
+
+        if segment and is_not_corrupt(segment):
+            # checksum = calculate_checksum(segment['sequence_number'])  # Replace with actual method to compute checksum
+            checksum = ""
+
+            send_ack(udp_socket, address,segment['sequence_number'], checksum)
+            if  has_sequence_number(segment, expected_seq_num):
+                print(f"Segment {segment['sequence_number']} received.")
+                expected_seq_num += 1  # Increment the expected sequence number
+            else:
+                print(f"Out-of-order segment received. Expected: {expected_seq_num}, got: {segment['sequence_number']}")
+             # process the segment and send ack
+            is_last_segment = process_segment(segment, received_segments)
+            if is_last_segment:
+                # here reassemble the file and save it
+                reassemble_file(received_segments, output_directory, segment['file_id'],20)
+                print(f"File {segment['file_id']} reassembled and saved.")
+
+        # If the packet is not the one we expect, we do nothing and wait for the next one
+        # The FSM diagram shows no action in the case of default (unexpected packet)
+
+
+def start_server():
+    local_ip = "server"
+    # local_ip = "127.0.0.1"
+    local_port = 8000
+    udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    udp_socket.bind((local_ip, local_port))
+
+    print("UDP server up and listening")
+
+    GBN_receiver(udp_socket)
+
+    udp_socket.close()
+    """
 
     while True:
         buffer_size = 4096
@@ -113,8 +201,7 @@ def start_server():
             # here reassemble the file and save it
             reassemble_file(received_segments, output_directory, segment['file_id'],20)
             print(f"File {segment['file_id']} reassembled and saved.")
-
-    udp_socket.close()
+    """
 
 if __name__ == "__main__":
     start_server()
